d76f55055af89421e76f2fcaca719a970cdea2b4
FNSL1996 <69156394+FNSL1996@users.noreply.github.com>
2020-08-29 19:16:59 -0400

    Update tensor_methods.py

--- fem2geo_lib/tensor_methods.py
+++ fem2geo_lib/tensor_methods.py
@@ -38,9 +38,102 @@ def rottensor(tensor,angle,eje):
     #Input: Tensor (array), ángulo de rotación en grados y eje 
     #Output: Tensor rotado
     R = rotmatrix(angle,eje)
-    rotT = np.dot(np.transpose(R),np.dot(tensor,R))
+    rotT = np.dot(R,np.dot(tensor,R.T))
     return rotT
 
+
+def resshear_enu(plane, sigma):
+    
+    np.seterr(divide='ignore', invalid='ignore')
+    """
+    Get resolved shear of a stress tensor onto a plane.
+    Input:
+        - plane[str/dip]
+        - tensor[3x3 np.ndarray] --> solid-mechanics sign convention
+    
+    Output:
+    *Note: To avoid ambiguity between full/half-azimutal measures, vectors are 
+    given in both a direction tau_hat (in the respective coordinate system), 
+    and a scalar tau, to express both sense and magnitude of the stress vector.
+    
+        - tau:   Resolved shear stress magnitude. If tau is positive,
+                 it represents the resolved stress onto the footwall 
+                 ("related" to reverse-kinematics), elsewise is onto the 
+                 hanging wall ("related" to normal-kinematics)
+        - tau_hat: Resolver shear stress direction in ENU coordinates 
+                   (physically pointing upwards)                 
+    """
+    n = tr.plane_sphe2enu(plane)
+    t = np.dot(sigma,n)
+
+    t_n = np.dot(t,n)*n
+    t_s = t - t_n  
+    
+    t_s_mag = np.linalg.norm(t_s)
+    t_s_dir = t_s/t_s_mag
+    if t_s_dir[2] < 0:
+        t_s_mag *= -1
+        t_s_dir *= -1
+        
+    return t_s_mag, t_s_dir
+
+
+def get_slip_tendency(sigma, p_disc):
+    
+    '''
+    Input: Tensor [3x3 np.ndarray]
+           p_disc [nx2 np.ndarray: str/dip]
+    '''
+    D = []
+    
+    if p_disc.ndim == 1:
+        n = tr.plane_sphe2enu(p_disc)
+        t = np.dot(sigma,n)        
+        sigma_n = np.dot(t,n)
+        sigma_s, sigma_s_dir = resshear_enu(p_disc,sigma) 
+        
+        return abs(sigma_s/sigma_n)
+    else:
+        for plane in p_disc:
+            n = tr.plane_sphe2enu(plane)
+            t = np.dot(sigma,n)        
+            sigma_n = np.dot(t,n)
+            sigma_s, sigma_s_dir = resshear_enu(plane,sigma)
+        
+            D.append(abs(sigma_s/sigma_n))
+            
+        return np.array(D)
+    
+    
+def plot_slip_tendency(sigma, n_strikes=181, n_dips=46, plot_eigenvec=False):
+    
+
+    Val, Vec = np.linalg.eig(sigma)
+    
+    Val = np.sort(Val)      ## Sort list of eigen values: minimum is s1
+    Vec = Vec[:, np.argsort(Val)]  ## Sort eigen vectors
+    
+    strikes = np.linspace(0, 360, n_strikes, endpoint=True) ## every 2 angles
+    dips = np.linspace(0, 90, n_dips, endpoint=True)  ## every 2 angles
+    
+    mesh_strikes, mesh_dips = np.meshgrid(strikes, dips)
+    plane_data = np.array([([i[0], i[1]]) for i 
+                                         in np.nditer((mesh_strikes, mesh_dips))])
+    
+    D = get_slip_tendency(sigma, plane_data)
+    lon, lat = mpl.pole(mesh_strikes, mesh_dips)
+    D_reshaped = D.reshape(mesh_strikes.shape)
+    
+
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111, projection='stereonet')
+    ax.grid()
+    cax = ax.pcolormesh(lon, lat, D_reshaped, cmap='jet', shading='auto')
+    
+    cbaxes = fig.add_axes([0.92, 0.1, 0.03, 0.3]) #Add additional axis for colorbar
+    fig.colorbar(cax, cax = cbaxes, shrink = 0.4)
+    
+    return fig, ax, D_reshaped, (mesh_strikes, mesh_dips)
 
 
 def get_dilation_tendency(sigma, n_disc):
